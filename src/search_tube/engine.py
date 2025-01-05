4da76e4cdad121f3349aa84d531229027115c88d
Oleh <olehluchkiv@MacBook-Pro-Nataliia.local>
2025-01-04 19:48:04 -0600

    wip: downloading works

--- src/search_tube/engine.py
+++ src/search_tube/engine.py
@@ -1,26 +1,31 @@
 from .storage import Storage
 from pytube import YouTube
+from pytubefix import YouTube
 
-SEARCH_TUBE_DB_NAME = "search_tube.db"
+YOUTUBE_PREFIX = 'https://www.youtube.com/watch?v='
 
 class Engine:
-    def __init__(self) -> None:
-        self.storage = Storage(SEARCH_TUBE_DB_NAME)
+    def __init__(self, db_name, downaloads_dir) -> None:
+        self.storage = Storage(db_name)
+        self.output_dir = downaloads_dir 
 
     def load_urls(self, urls: list[str]) -> None:
         self.storage.save_urls(urls)
         self.process()
     
     def process(self):
-        urls = self.storage.retrieve_urls()
-        for u in urls[:1]:
+        youtube_ids = self.storage.retrieve_urls(downloaded=False)
+        for youtube_id in youtube_ids[:1]:
             # TODO: running only for first URL! to be continued...
-
-            print("processing ", u)
-
             try:
-                yt = YouTube(u)
-                print("title:", yt.title)
+                url = YOUTUBE_PREFIX + youtube_id
+                yt = YouTube(url)
+                self.storage.save_title(youtube_id, yt.title)
+                ys = yt.streams.get_audio_only()
+                filename = youtube_id + '.mp4'
+                ys.download(output_path=self.output_dir, filename=filename)
+                self.storage.make_url_downloaded(youtube_id)
+                print("title:", yt.title, "downloaded")
             except Exception as e:
                 print(f"Error downloading video: {e}")
                 continue
